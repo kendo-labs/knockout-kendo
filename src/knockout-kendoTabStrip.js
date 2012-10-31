d0bcc80ebd2034e90900af3d28d91a1d308e02bf
Ryan Niemeyer <rniemeyer@gmail.com>
2012-10-30 22:58:08 -0500

    Remove "selected" live option from tabStrip, as it does not work properly (does not synchronize with other tabs) and cannot be fixed in the current system easily (#34)

--- src/knockout-kendoTabStrip.js
+++ src/knockout-kendoTabStrip.js
@@ -7,17 +7,8 @@ createBinding({
     name: "kendoTab",
     parent: "kendoTabStrip",
     watch: {
-        selected: function(element, value) {
-            this.select(value ? element : null);
-        },
         enabled: ENABLE
     },
     childProp: "item",
-    events: {
-        selected: {
-            writeTo: SELECTED,
-            value: true
-        }
-    },
     async: true
 });

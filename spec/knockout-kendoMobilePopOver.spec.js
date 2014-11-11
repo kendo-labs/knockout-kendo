ab4857e1f9b4fe5f4325ff408049f9f244457fa4
Ryan Niemeyer <rniemeyer@gmail.com>
2014-11-10 18:07:05 -0600

    kendoMobilePopOver spec

---
+++ spec/knockout-kendoMobilePopOver.spec.js
@@ -0,0 +1,9 @@
+describe("ko.bindingHandlers.kendoMobilePopOver", function(){
+    //standard cases
+    ko.kendo.generateBindingSpecs("kendoMobilePopOver", {
+        html: "<div><div data-role=\"view\"></div></div>"
+    });
+
+    //additional kendoMobilePopOver cases
+
+});
